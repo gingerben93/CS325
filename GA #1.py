d95674b78c433845f7d4eb9a5861cfa84b02ee9d
dochickensfly <philosopher.notdummy@gmail.com>
2015-01-14 23:56:52 -0800

    Timing

--- GA #1.py
+++ GA #1.py
@@ -3,6 +3,9 @@
 #CS325
 #Assignment #1
 
+import random
+import time
+
 #Enumeration
 def alg1(arr):
     sum1 = arr[0]
@@ -12,13 +15,13 @@ def alg1(arr):
         for i in range(0,len(arr)):
             #print(j,i)
             sum2 = 0
-            for k in range(i,j):
+            for k in range(i,j+1):
                 sum2 = sum2 + arr[k]
                 count = count + 1
                 if sum2 > sum1:
                     sum1 = sum2
 
-    print(sum1,count)
+    #print(sum1,count)
 
 #Better Enumeration
 def alg2(arr):
@@ -45,16 +48,26 @@ def alg3(arr):
         max1 = max(arr[x], max1 + arr[x])
         max2 = max(max2, max1)
         count = count + 1
-        print (x)
         
     print(max2, count)
 
 #main function
 def main():
-    a = [31,-41,59,26,-53,58,97,-93,-23,84]
-    alg1(a)
-    alg2(a)
-    alg3(a)
+    #a = [random.randint(-100,100) for _ in range (100)]
+    #alg1(a)
+    #alg2(a)
+    #alg3(a)
+
+    spare = [0,0,0,0,0,0,0,0,0,0]
+
+    for j in range(1,9):
+        for i in range(0,10):
+            a = [random.randint(-100,100) for _ in range (j*100)]
+            spare[i] = time.time()
+            alg1(a)
+            spare[i] = time.time() - spare[i]
+        print(j*100,sum(spare)/10)
 
 #program
 main()
+
